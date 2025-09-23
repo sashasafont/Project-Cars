69fb47237b8c7a4090483a307b10158fb0838d45
Sasha <safontsanek@gmail.com>
2025-09-24 00:20:11 +0200

    Tablas modelo y fabricante añadidas y fact con los datos normalizados y limpios

--- project/db/sql/03_dim_fact.sql
+++ project/db/sql/03_dim_fact.sql
@@ -4,39 +4,41 @@ SELECT COUNT(*) FROM ventacoches.coches_raw;
 SELECT * FROM ventacoches.coches_raw limit 5;
 
 --cuenta provincias distintas(52)
-SELECT count(DISTINCT provincia)
+SELECT COUNT(DISTINCT provincia)
 FROM ventacoches.coches_raw;
 --seleciona las provincias
 SELECT DISTINCT provincia
 FROM ventacoches.coches_raw;
 
---cuenta marca -> sólo hay una, la eliminaremos
-SELECT count(DISTINCT marca)
-FROM ventacoches.coches_raw;
-
 --cuenta fabricante (75) son muchos????
-SELECT count(DISTINCT fabricante)
+SELECT COUNT(DISTINCT fabricante)
 FROM ventacoches.coches_raw;
 --seleciona fabricantes
 SELECT DISTINCT fabricante
 FROM ventacoches.coches_raw;
 
+SELECT COUNT(DISTINCT modelo)
+FROM ventacoches.coches_raw;
+--seleciona modelos
+SELECT DISTINCT modelo
+FROM ventacoches.coches_raw;
+
 --cuenta fuel (7) -> foreing
-SELECT count(DISTINCT fuel)
+SELECT COUNT(DISTINCT fuel)
 FROM ventacoches.coches_raw;
 --seleciona fuel -> foreing
 SELECT DISTINCT fuel
 FROM ventacoches.coches_raw;
  
 --cuenta puertas (4) ->foreing
-SELECT count(DISTINCT puertas)
+SELECT COUNT(DISTINCT puertas)
 FROM ventacoches.coches_raw;
 --seleciona puertas
 SELECT DISTINCT puertas
 FROM ventacoches.coches_raw;
 
 --cuenta tipo (2)
-SELECT count(DISTINCT tipo)
+SELECT COUNT(DISTINCT tipo)
 FROM ventacoches.coches_raw;
 --seleciona tipo
 SELECT DISTINCT tipo
@@ -47,20 +49,35 @@ CREATE TABLE ventacoches.dim_provincias(
 provincia_id SERIAL PRIMARY KEY,
 provincia TEXT UNIQUE);
 
+DROP TABLE IF EXISTS ventacoches.dim_fabricantes;
+CREATE TABLE ventacoches.dim_fabricantes(
+fabricante_id SERIAL PRIMARY KEY,
+fabricante TEXT UNIQUE
+);
+
+DROP TABLE IF EXISTS ventacoches.dim_modelos;
+CREATE TABLE ventacoches.dim_modelos(
+	modelo_id SERIAL PRIMARY KEY,
+	modelo TEXT UNIQUE
+);
+
 DROP TABLE IF EXISTS ventacoches.dim_fuel;
 CREATE TABLE ventacoches.dim_fuel(
 fuel_id SERIAL PRIMARY KEY,
-fuel TEXT UNIQUE);
+fuel TEXT UNIQUE
+);
 
 DROP TABLE IF EXISTS ventacoches.dim_puertas;
 CREATE TABLE ventacoches.dim_puertas(
 puertas_id SERIAL PRIMARY KEY,
-puertas_numero INT UNIQUE);
+puertas_numero INT UNIQUE
+);
 
 DROP TABLE IF EXISTS ventacoches.dim_tipo;
 CREATE TABLE ventacoches.dim_tipo(
 tipo_id SERIAL PRIMARY KEY,
-tipo_nombre TEXT UNIQUE);
+tipo_nombre TEXT UNIQUE
+);
 
 
 INSERT INTO ventacoches.dim_provincias (provincia)
@@ -71,6 +88,19 @@ FROM
 WHERE
     provincia IS NOT NULL;
 
+INSERT INTO ventacoches.dim_fabricantes (fabricante)
+SELECT DISTINCT
+	NULLIF(regexp_replace(lower(coalesce(fabricante,'')), '\s+',' ','g'),'')
+FROM ventacoches.coches_raw
+WHERE
+	fabricante IS NOT NULL;
+
+INSERT INTO ventacoches.dim_modelos (modelo)
+SELECT DISTINCT
+	NULLIF(regexp_replace(lower(coalesce(modelo,'')), '\s+',' ','g'),'')
+FROM ventacoches.coches_raw
+WHERE
+	modelo IS NOT NULL;
 
 INSERT INTO ventacoches.dim_fuel (fuel)
 SELECT DISTINCT
@@ -80,14 +110,6 @@ FROM
 WHERE
 	fuel IS NOT NULL;
 
-INSERT INTO ventacoches.dim_tipo (tipo_nombre)
-SELECT DISTINCT
-    NULLIF(regexp_replace(lower(coalesce(tipo,'')), '\s+',' ','g'),'')
-FROM
-    ventacoches.coches_raw
-WHERE
-    tipo IS NOT NULL;
-
 INSERT INTO ventacoches.dim_puertas (puertas_numero)
 SELECT DISTINCT 
 	puertas 
@@ -96,29 +118,59 @@ FROM
 WHERE 
 	puertas IS NOT NULL;
 
+INSERT INTO ventacoches.dim_tipo (tipo_nombre)
+SELECT DISTINCT
+    NULLIF(regexp_replace(lower(coalesce(tipo,'')), '\s+',' ','g'),'')
+FROM
+    ventacoches.coches_raw
+WHERE
+    tipo IS NOT NULL;
 
 DROP TABLE IF EXISTS ventacoches.coches;
 CREATE TABLE ventacoches.coches(
 	id SERIAL PRIMARY KEY,
-	
 	fecha DATE,
 	cliente TEXT,
 	email TEXT,
-	fabricante TEXT,
-	modelo TEXT,
+
+	fabricante_id INT REFERENCES ventacoches.dim_fabricantes(fabricante_id),
+	modelo_id INT REFERENCES ventacoches.dim_modelos(modelo_id),
 	version TEXT,
+	
+	fuel_id INT REFERENCES ventacoches.dim_fuel(fuel_id),
+	puertas_id INT REFERENCES ventacoches.dim_puertas(puertas_id),
+	tipo_id INT REFERENCES ventacoches.dim_tipo(tipo_id),
+	provincia_id INT REFERENCES ventacoches.dim_provincias(provincia_id),
+
 	precio INT,
+	precio_financiado INT,
 	cantidad INT
 );
 
-INSERT INTO ventacoches.coches (fecha, cliente, email, fabricante, modelo, version, precio, cantidad)
+INSERT INTO ventacoches.coches (
+	fecha, cliente, email, 
+	fabricante_id, modelo_id, version,
+	fuel_id, puertas_id, tipo_id, provincia_id,
+	precio, precio_financiado, cantidad
+	)
 SELECT
-	CAST(fecha AS DATE),
-	cliente,
-	email,
-	fabricante,
-	modelo,
-	version,
-	CAST(precio AS INT),
-	CAST(cantidad AS INT)
-FROM ventacoches.staging_copy;
+	CAST(s.fecha_publicacion AS DATE),
+	NULL AS cliente,
+	NULL AS email,
+	f.fabricante_id,
+	m.modelo_id,
+	s.version,
+	fu.fuel_id,
+	p.puertas_id,
+	t.tipo_id,
+	pr.provincia_id,
+	CAST(s.precio AS INT),
+	CAST(s.precio_financiado AS INT),
+	1 AS cantidad
+FROM ventacoches.coches_raw s
+LEFT JOIN ventacoches.dim_fabricantes f ON lower(s.fabricante) = f.fabricante
+LEFT JOIN ventacoches.dim_modelos m ON lower(s.modelo) = m.modelo
+LEFT JOIN ventacoches.dim_fuel fu ON lower(s.fuel) = fu.fuel
+LEFT JOIN ventacoches.dim_puertas p ON s.puertas = p.puertas_numero
+LEFT JOIN ventacoches.dim_tipo t ON lower(s.tipo) = t.tipo_nombre
+LEFT JOIN ventacoches.dim_provincias pr ON lower(s.provincia) = pr.provincia;
